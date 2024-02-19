03998939332aa9be666a0bdb9f77e58468000a19
ppsingh <ppsingh.iitk@gmail.com>
2024-02-19 20:33:01 +0100

    add packages info

--- setup.py
+++ setup.py
@@ -1,19 +1,11 @@
 import setuptools
-# import glob
-# def get_package_data():
-#         list_of_files = []
-#         list_of_files = list_of_files + glob.glob('appstore/docStore/img/*.png')
-#         list_of_files = list_of_files + glob.glob('appstore/docStore/ndcs/*.txt')
-#         list_of_files = list_of_files + glob.glob('appstore/docStore/sample/*.txt')
-#         list_of_files = list_of_files + glob.glob('appstore/docStore/sample/*.json')
-#         return list_of_files
 
 
 install_requires=[
         "st-annotated-text==3.0.0",
         "markdown==3.4.1",
-        "streamlit-aggrid",
-        "streamlit_option_menu",
+        "streamlit-aggrid==0.3.4",
+        "streamlit_option_menu==0.3.12",
 ]
 
 
@@ -25,9 +17,5 @@ setuptools.setup(
         author_email='prashant.singh@giz.de',
         package_dir={"": "src"},
         packages=setuptools.find_packages(where='src'),  
-        # package_data={
-        # "appstore":['data/*.png','data/*.txt','data/*.json']},
-        # data_files=[('ndcs',['src/appstore/docStore/ndcs/cca.txt','src/appstore/docStore/ndcs/ccm.txt',
-        # 'src/appstore/docStore/ndcs/countryList.txt'])],
         install_requires=install_requires, #external packages as dependencies
         )
